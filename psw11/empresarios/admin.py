2b59d9262152359a1ec9f0d4cb6f3d154a91aace
joschonarth <joschonarth@gmail.com>
2024-08-10 14:27:48 -0300

    Update PSW11

--- psw11/empresarios/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Empresas, Documento, Metricas
-
-admin.site.register(Empresas)
-admin.site.register(Documento)
-admin.site.register(Metricas)
--- psw11/investidores/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InvestidoresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'investidores'
